1724f3f5781f06f9647a5a53863805699b79292e
franciscalaureano <mariakikalaureano1@gmail.com>
2021-11-29 09:45:55 +0000

    acrescentei "a"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>kika e angela.</p>
+        <p>kika e a angela.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
