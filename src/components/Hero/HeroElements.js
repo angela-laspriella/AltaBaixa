6a4ee2087ec3c91009c038516e499336a629cd28
franciscalaureano <mariakikalaureano1@gmail.com>
2021-12-01 17:25:02 +0000

    kika

---
+++ src/components/Hero/HeroElements.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HeroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  overflow: hidden;
+  color: black;
+`;
+
+export const HeroContent = styled.div`
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+`;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "../components/Hero";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
